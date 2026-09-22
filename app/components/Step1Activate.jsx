b12b03664f73ceb87d0f70db042da0b92ca6f986
Rishabh Patel <rishabh.patel@brainvire.com>
2026-09-22 12:54:08 +0530

    performance-app-deploy1

--- app/components/Step1Activate.jsx
+++ app/components/Step1Activate.jsx
@@ -207,7 +207,7 @@ export default function Step1Activate({
                 marginBottom: 4,
               }}
             >
-              A powerful speed app without the expensive price tag1?
+              A powerful speed app without the expensive price tag?
             </div>
             <div style={{ fontSize: 13, color: "#4A5568" }}>
               We&apos;re committed to helping Shopify merchants build faster,
